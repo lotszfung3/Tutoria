d3f91643b440c5731ca9471fb527b87f26dda2ba
Frankie Lo <frankielo@Frankies-MacBook-Pro.local>
2017-10-31 18:41:16 +0800

    minor stuff

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -1,16 +1,17 @@
 from django.conf.urls import url
 
 from . import views
+from . import views_account
 
 urlpatterns = [
-    url(r'^test$', views.test, name='test'),
+    url(r'^test$', views_account.test, name='test'),
     url(r'^findTutors$',views.findTutors,name='findTutors'),
     url(r'^tutorsList',views.tutorsList,name='tutorsList'),
     url(r'^detailedProfile',views.detailedProfile,name='detailedProfile'),
     url(r'^confirmPayment$',views.confirmPayment,name='confirmPayment'),
     url(r'^(?P<student_ID>\d+)/upcomingSessions',views.viewUpcomingSessions,name='viewUpcomingSessions'),
     url(r'^(?P<student_ID>\d+)/cancelSession/(?P<session_ID>\d+)',views.cancelSession,name='cancelSession'),
-    url(r'^register$',views.register,name='register'),
-	url(r'^login$',views.login_h,name='login'),
-	url(r'^logout$',views.logout_h,name='logout')
+    url(r'^register$',views_account.register,name='register'),
+	url(r'^login$',views_account.login_h,name='login'),
+	url(r'^logout$',views_account.logout_h,name='logout')
 ]
--- mainApp/views.py
+++ mainApp/views.py
@@ -4,39 +4,11 @@ from .models import *
 from datetime import date
 from django.contrib.auth import authenticate, login,logout
 from django.shortcuts import HttpResponseRedirect
-
 from django.contrib.auth.decorators import login_required
 
-def test(request):
-    return render(request, 'mainApp/test.html')
-@login_required
-def logout_h(request):
-	if(request.user and request.user.is_authenticated()):
-		logout(request)
-	return HttpResponseRedirect('/main/login')
-
-def login_h(request):
-	if request.method == 'GET':
-		return render(request, 'mainApp/login.html')
-	elif request.method == 'POST':
-		username=request.POST['username']
-		password=request.POST['password']
-		user=authenticate(request,username=username,password=password)
-		if user is not None:
-			login(request,user)
-			return HttpResponseRedirect('./findTutors')
-		else:
-			return HttpResponseRedirect('./login')
-
-def register(request):
-	if request.method == 'GET':
-		return HttpResponse("register_get")
-	elif request.method == 'POST':
-		return HttpResponse("register_post")
-#form for finding tutors
 @login_required
 def findTutors(request):
-	return render(request,'mainApp/findTutors.html')
+	return render(request,'mainApp/findTutors.html',{'std':request.user.student})
 
 #list of tutors with requirement in side request.GET
 @login_required
