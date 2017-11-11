fa7b288d4d3af19c085272eba6cb12f6bb7ed314
Teddy Riker <teddy+rails@brokerbuddha.com>
2017-11-11 16:34:40 +0800

    need to notify tutor of transaction

--- mainApp/crons.py
+++ mainApp/crons.py
@@ -5,6 +5,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from datetime import datetime,timezone, timedelta
 from django_cron import CronJobBase, Schedule
+from django.core.mail import send_mail
 
 class Update_Session_States(CronJobBase):
     RUN_EVERY_MINS = 30
@@ -18,12 +19,22 @@ class Update_Session_States(CronJobBase):
     		if x.state=='normal':
     			new_lst.append(x)
     	return new_lst
+    def notify_tutor(tutor, session):
+    	send_mail(
+    		'Your MyTutors payment has been received!',
+    		'Here is the message.',
+    		'from@example.com',
+    		['to@example.com'],
+    		fail_silently=False,
+		)
     def do(self):
     	normal_sessions = get_normal_sessions(all_sessions)
     	for session in normal_sessions:
     		if (session.session_datetime + timedelta(hours=1)) < datetime.now(timezone('UTC')):
     			session.state='ended'
+    			#TODO: commission system for mytutors
     			session.session_tutor.amount = session.session_tutor.amount + session.session_tutor.hourly_rate
+    			notify_tutor(session.session_turor, session)
     			session.session_tutor.save()
     			session.save()
     		elif (session.session_datetime - timedelta(hours=24)) > datetime.now(timezone('UTC')):
@@ -47,4 +58,5 @@ class Update_Transaction_States(CronJobBase):
 		for transaction in pending_transactions:
 			if(transaction.involved_session.state=='ended'):
 				transaction.state='completed'
+				#TODO: send email notification to Tutor that they have received payment
 				transaction.save()
